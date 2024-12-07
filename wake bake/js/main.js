b62ec66b14d653ca7b29b64a0a52ec7e4c8916cd
Anudas108 <stashkovalexey1@gmail.com>
2024-12-07 10:47:52 +0300

    Add files via upload

--- wake bake/js/main.js
+++ wake bake/js/main.js
@@ -1,6 +1,11 @@
+// ----------------------бургер-меню
+
 (function(){
+
   document.addEventListener('click', burgerInit)
+
   function burgerInit(e) {
+    
     const burgerIcon = e.target.closest('.burger-icon')
     const burgerNavLink = e.target.closest('.nav__link')
 
@@ -11,11 +16,28 @@
       document.body.classList.add('body--opened-menu')
     } else {
       document.body.classList.remove('body--opened-menu')
-
     }
-
-    
   }
-
-
 })()
+
+// ---------------------модалка
+
+const modal = document.querySelector('.modal')
+const modalBtn = document.querySelector('.about__img-button')
+
+modalBtn.addEventListener('click', openModal)
+
+function openModal(e) {
+  e.preventDefault()
+  document.body.classList.toggle('body--opened-modal')
+}
+
+modal.addEventListener('click', closeModal)
+
+function closeModal(e) {
+  e.preventDefault()
+  const target = e.target
+  if (target.closest('.modal__cansel') || target.classList.contains('modal'))
+    document.body.classList.remove('body--opened-modal')
+}
+
