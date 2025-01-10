bb089b0f92474c66fd046dc434c1600db330cfeb
Anudas108 <stashkovalexey1@gmail.com>
2025-01-10 17:06:15 +0300

    Add files via upload

--- Дипломка/js/main.js
+++ Дипломка/js/main.js
@@ -6,148 +6,244 @@ const themeToggleButton3 = document.querySelector('.price__section-button-white'
 const themeToggleButton4 = document.querySelector('.price__section-button-black');
 
 themeToggleButton.addEventListener('click', () => {
-  if (document.body.classList.contains('dark-theme'))
-     {
+  if (document.body.classList.contains('dark-theme')) {
       document.body.classList.remove('dark-theme');
       document.body.classList.add('light-theme');
       themeToggleButton.innerText = 'Белый';
       localStorage.setItem('theme', 'light-theme');
 
-      headerScooter.style.opacity = 0; // Скрываем изображение
-      setTimeout(() => {
-        headerScooter.src = '../img/scooter/scooter-white.png'; // Меняем изображение
-        headerScooter.style.opacity = 1; // Показываем изображение
-      }, 300);
-      App.style.opacity = 0; // Скрываем изображение
-      setTimeout(() => {
-        App.src = '../img/logo/app-store-white.svg'; // Меняем изображение
-        App.style.opacity = 1; // Показываем изображение
-      }, 300);
-      Google.style.opacity = 0; // Скрываем изображение
-      setTimeout(() => {
-        Google.src = '../img/logo/google-white.svg'; // Меняем изображение
-        Google.style.opacity = 1; // Показываем изображение
-      }, 300);
-      telefhoneColor.style.opacity = 0; // Скрываем изображение
-      setTimeout(() => {
-        telefhoneColor.src = '../img/scooter/security__telefhone1.png'; // Меняем изображение
-        telefhoneColor.style.opacity = 1; // Показываем изображение
-      }, 300);
-      littleBikeDown.style.opacity = 0; // Скрываем изображение
-      setTimeout(() => {
-        littleBikeDown.src = '../img/scooter/scooter-white.png'; // Меняем изображение
-        littleBikeDown.style.opacity = 1; // Показываем изображение
-      }, 300);
-  } else return
-})
+      function changeImage(element, newSrc) {
+          element.style.opacity = 0; // Скрываем изображение
+          setTimeout(() => {
+              element.src = newSrc; // Меняем изображение
+              element.onload = () => { // Убедитесь, что изображение загружено
+                  element.style.opacity = 1; // Показываем изображение
+              };
+          }, 300);
+      }
+      changeImage(headerScooter, '../img/scooter/scooter-white.png');
+      changeImage(App, '../img/logo/app-store-white.svg');
+      changeImage(Google, '../img/logo/google-white.svg');
+      changeImage(telefhoneColor, '../img/scooter/security__telefhone1.png');
+      changeImage(littleBikeDown, '../img/scooter/scooter-white.png');
+  }
+});
+
+// themeToggleButton.addEventListener('click', () => {
+//   if (document.body.classList.contains('dark-theme'))
+//      {
+//       document.body.classList.remove('dark-theme');
+//       document.body.classList.add('light-theme');
+//       themeToggleButton.innerText = 'Белый';
+//       localStorage.setItem('theme', 'light-theme');
+
+//       headerScooter.style.opacity = 0; // Скрываем изображение
+//       setTimeout(() => {
+//         headerScooter.src = '../img/scooter/scooter-white.png'; // Меняем изображение
+//         headerScooter.style.opacity = 1; // Показываем изображение
+//       }, 300);
+//       App.style.opacity = 0; // Скрываем изображение
+//       setTimeout(() => {
+//         App.src = '../img/logo/app-store-white.svg'; // Меняем изображение
+//         App.style.opacity = 1; // Показываем изображение
+//       }, 300);
+//       Google.style.opacity = 0; // Скрываем изображение
+//       setTimeout(() => {
+//         Google.src = '../img/logo/google-white.svg'; // Меняем изображение
+//         Google.style.opacity = 1; // Показываем изображение
+//       }, 300);
+//       telefhoneColor.style.opacity = 0; // Скрываем изображение
+//       setTimeout(() => {
+//         telefhoneColor.src = '../img/scooter/security__telefhone1.png'; // Меняем изображение
+//         telefhoneColor.style.opacity = 1; // Показываем изображение
+//       }, 300);
+//       littleBikeDown.style.opacity = 0; // Скрываем изображение
+//       setTimeout(() => {
+//         littleBikeDown.src = '../img/scooter/scooter-white.png'; // Меняем изображение
+//         littleBikeDown.style.opacity = 1; // Показываем изображение
+//       }, 300);
+//   } else return
+// })
+
+// themeToggleButton2.addEventListener('click', () => {
+//   if (document.body.classList.contains('light-theme'))
+//      {
+//       document.body.classList.remove('light-theme');
+//       document.body.classList.add('dark-theme');
+//       themeToggleButton.innerText = 'Белый';
+//       localStorage.setItem('theme', 'dark-theme');
+
+//       headerScooter.style.opacity = 0; // Скрываем изображение
+//     setTimeout(() => {
+//       headerScooter.src = '../img/scooter/scooter-black.png'; // Меняем изображение
+//       headerScooter.style.opacity = 0.9; // Показываем изображение
+//     }, 300);
+//     App.style.opacity = 0; // Скрываем изображение
+//       setTimeout(() => {
+//         App.src = '/img/logo/app-store-black.svg'; // Меняем изображение
+//         App.style.opacity = 1; // Показываем изображение
+//       }, 300);
+//       Google.style.opacity = 0; // Скрываем изображение
+//       setTimeout(() => {
+//         Google.src = '/img/logo/google-black.svg'; // Меняем изображение
+//         Google.style.opacity = 1; // Показываем изображение
+//       }, 300);
+//       telefhoneColor.style.opacity = 0; // Скрываем изображение
+//       setTimeout(() => {
+//         telefhoneColor.src = '/img/scooter/security__telefhone-white.png'; // Меняем изображение
+//         telefhoneColor.style.opacity = 1; // Показываем изображение
+//       }, 300);
+//       littleBikeDown.style.opacity = 0; // Скрываем изображение
+//       setTimeout(() => {
+//         littleBikeDown.src = '/img/scooter/scooter-black.png'; // Меняем изображение
+//         littleBikeDown.style.opacity = 1; // Показываем изображение
+//       }, 300);
+//   } else return
+// })
 
 themeToggleButton2.addEventListener('click', () => {
-  if (document.body.classList.contains('light-theme'))
-     {
+  if (document.body.classList.contains('light-theme')) {
       document.body.classList.remove('light-theme');
       document.body.classList.add('dark-theme');
       themeToggleButton.innerText = 'Белый';
       localStorage.setItem('theme', 'dark-theme');
 
-      headerScooter.style.opacity = 0; // Скрываем изображение
-    setTimeout(() => {
-      headerScooter.src = '../img/scooter/scooter-black.png'; // Меняем изображение
-      headerScooter.style.opacity = 0.9; // Показываем изображение
-    }, 300);
-    App.style.opacity = 0; // Скрываем изображение
-      setTimeout(() => {
-        App.src = '../img/logo/app-store-black.svg'; // Меняем изображение
-        App.style.opacity = 1; // Показываем изображение
-      }, 300);
-      Google.style.opacity = 0; // Скрываем изображение
-      setTimeout(() => {
-        Google.src = '../img/logo/google-black.svg'; // Меняем изображение
-        Google.style.opacity = 1; // Показываем изображение
-      }, 300);
-      telefhoneColor.style.opacity = 0; // Скрываем изображение
-      setTimeout(() => {
-        telefhoneColor.src = '../img/scooter/security__telefhone-white.png'; // Меняем изображение
-        telefhoneColor.style.opacity = 1; // Показываем изображение
-      }, 300);
-      littleBikeDown.style.opacity = 0; // Скрываем изображение
-      setTimeout(() => {
-        littleBikeDown.src = '../img/scooter/scooter-black.png'; // Меняем изображение
-        littleBikeDown.style.opacity = 1; // Показываем изображение
-      }, 300);
-  } else return
-})
+      function changeImage(element, newSrc) {
+          element.style.opacity = 0; // Скрываем изображение
+          setTimeout(() => {
+              element.src = newSrc; // Меняем изображение
+              element.onload = () => { // Убедитесь, что изображение загружено
+                  element.style.opacity = 1; // Показываем изображение
+              };
+          }, 300);
+      }
+      changeImage(headerScooter, '../img/scooter/scooter-black.png');
+      changeImage(App, '../img/logo/app-store-black.svg');
+      changeImage(Google, '../img/logo/google-black.svg');
+      changeImage(telefhoneColor, '../img/scooter/security__telefhone-white.png');
+      changeImage(littleBikeDown, '../img/scooter/scooter-black.png');
+  }
+});
 
 themeToggleButton3.addEventListener('click', () => {
-  if (document.body.classList.contains('dark-theme'))
-     {
+  if (document.body.classList.contains('dark-theme')) {
       document.body.classList.remove('dark-theme');
       document.body.classList.add('light-theme');
       themeToggleButton.innerText = 'Белый';
       localStorage.setItem('theme', 'light-theme');
 
-      headerScooter.style.opacity = 0; // Скрываем изображение
-      setTimeout(() => {
-        headerScooter.src = '../img/scooter/scooter-white.png'; // Меняем изображение
-        headerScooter.style.opacity = 1; // Показываем изображение
-      }, 300);
-      App.style.opacity = 0; // Скрываем изображение
-      setTimeout(() => {
-        App.src = '../img/logo/app-store-white.svg'; // Меняем изображение
-        App.style.opacity = 1; // Показываем изображение
-      }, 300);
-      Google.style.opacity = 0; // Скрываем изображение
-      setTimeout(() => {
-        Google.src = '../img/logo/google-white.svg'; // Меняем изображение
-        Google.style.opacity = 1; // Показываем изображение
-      }, 300);
-      telefhoneColor.style.opacity = 0; // Скрываем изображение
-      setTimeout(() => {
-        telefhoneColor.src = '../img/scooter/security__telefhone1.png'; // Меняем изображение
-        telefhoneColor.style.opacity = 1; // Показываем изображение
-      }, 300);
-      littleBikeDown.style.opacity = 0; // Скрываем изображение
-      setTimeout(() => {
-        littleBikeDown.src = '../img/scooter/scooter-white.png'; // Меняем изображение
-        littleBikeDown.style.opacity = 1; // Показываем изображение
-      }, 300);
-  } else return
-})
+      function changeImage(element, newSrc) {
+          element.style.opacity = 0; // Скрываем изображение
+          setTimeout(() => {
+              element.src = newSrc; // Меняем изображение
+              element.onload = () => { // Убедитесь, что изображение загружено
+                  element.style.opacity = 1; // Показываем изображение
+              };
+          }, 300);
+      }
+      changeImage(headerScooter, '../img/scooter/scooter-white.png');
+      changeImage(App, '../img/logo/app-store-white.svg');
+      changeImage(Google, '../img/logo/google-white.svg');
+      changeImage(telefhoneColor, '../img/scooter/security__telefhone1.png');
+      changeImage(littleBikeDown, '../img/scooter/scooter-white.png');
+  }
+});
+
+// themeToggleButton3.addEventListener('click', () => {
+//   if (document.body.classList.contains('dark-theme'))
+//      {
+//       document.body.classList.remove('dark-theme');
+//       document.body.classList.add('light-theme');
+//       themeToggleButton.innerText = 'Белый';
+//       localStorage.setItem('theme', 'light-theme');
+
+//       headerScooter.style.opacity = 0; // Скрываем изображение
+//       setTimeout(() => {
+//         headerScooter.src = '../img/scooter/scooter-white.png'; // Меняем изображение
+//         headerScooter.style.opacity = 1; // Показываем изображение
+//       }, 300);
+//       App.style.opacity = 0; // Скрываем изображение
+//       setTimeout(() => {
+//         App.src = '../img/logo/app-store-white.svg'; // Меняем изображение
+//         App.style.opacity = 1; // Показываем изображение
+//       }, 300);
+//       Google.style.opacity = 0; // Скрываем изображение
+//       setTimeout(() => {
+//         Google.src = '../img/logo/google-white.svg'; // Меняем изображение
+//         Google.style.opacity = 1; // Показываем изображение
+//       }, 300);
+//       telefhoneColor.style.opacity = 0; // Скрываем изображение
+//       setTimeout(() => {
+//         telefhoneColor.src = '../img/scooter/security__telefhone1.png'; // Меняем изображение
+//         telefhoneColor.style.opacity = 1; // Показываем изображение
+//       }, 300);
+//       littleBikeDown.style.opacity = 0; // Скрываем изображение
+//       setTimeout(() => {
+//         littleBikeDown.src = '../img/scooter/scooter-white.png'; // Меняем изображение
+//         littleBikeDown.style.opacity = 1; // Показываем изображение
+//       }, 300);
+//   } else return
+// })
 
 themeToggleButton4.addEventListener('click', () => {
-  if (document.body.classList.contains('light-theme'))
-     {
+  if (document.body.classList.contains('light-theme')) {
       document.body.classList.remove('light-theme');
       document.body.classList.add('dark-theme');
       themeToggleButton.innerText = 'Белый';
       localStorage.setItem('theme', 'dark-theme');
 
-      headerScooter.style.opacity = 0; // Скрываем изображение
-    setTimeout(() => {
-      headerScooter.src = '../img/scooter/scooter-black.png'; // Меняем изображение
-      headerScooter.style.opacity = 0.9; // Показываем изображение
-    }, 300);
-    App.style.opacity = 0; // Скрываем изображение
-      setTimeout(() => {
-        App.src = '../img/logo/app-store-black.svg'; // Меняем изображение
-        App.style.opacity = 1; // Показываем изображение
-      }, 300);
-      Google.style.opacity = 0; // Скрываем изображение
-      setTimeout(() => {
-        Google.src = '../img/logo/google-black.svg'; // Меняем изображение
-        Google.style.opacity = 1; // Показываем изображение
-      }, 300);
-      telefhoneColor.style.opacity = 0; // Скрываем изображение
-      setTimeout(() => {
-        telefhoneColor.src = '../img/scooter/security__telefhone-white.png'; // Меняем изображение
-        telefhoneColor.style.opacity = 1; // Показываем изображение
-      }, 300);
-      littleBikeDown.style.opacity = 0; // Скрываем изображение
-      setTimeout(() => {
-        littleBikeDown.src = '../img/scooter/scooter-black.png'; // Меняем изображение
-        littleBikeDown.style.opacity = 1; // Показываем изображение
-      }, 300);
-  } else return
-})
+      function changeImage(element, newSrc) {
+          element.style.opacity = 0; // Скрываем изображение
+          setTimeout(() => {
+              element.src = newSrc; // Меняем изображение
+              element.onload = () => { // Убедитесь, что изображение загружено
+                  element.style.opacity = 1; // Показываем изображение
+              };
+          }, 300);
+      }
+      changeImage(headerScooter, '../img/scooter/scooter-black.png');
+      changeImage(App, '../img/logo/app-store-black.svg');
+      changeImage(Google, '../img/logo/google-black.svg');
+      changeImage(telefhoneColor, '../img/scooter/security__telefhone-white.png');
+      changeImage(littleBikeDown, '../img/scooter/scooter-black.png');
+  }
+});
+
+// themeToggleButton4.addEventListener('click', () => {
+//   if (document.body.classList.contains('light-theme'))
+//      {
+//       document.body.classList.remove('light-theme');
+//       document.body.classList.add('dark-theme');
+//       themeToggleButton.innerText = 'Белый';
+//       localStorage.setItem('theme', 'dark-theme');
+
+//       headerScooter.style.opacity = 0; // Скрываем изображение
+//     setTimeout(() => {
+//       headerScooter.src = '../img/scooter/scooter-black.png'; // Меняем изображение
+//       headerScooter.style.opacity = 0.9; // Показываем изображение
+//     }, 300);
+//     App.style.opacity = 0; // Скрываем изображение
+//       setTimeout(() => {
+//         App.src = '../img/logo/app-store-black.svg'; // Меняем изображение
+//         App.style.opacity = 1; // Показываем изображение
+//       }, 300);
+//       Google.style.opacity = 0; // Скрываем изображение
+//       setTimeout(() => {
+//         Google.src = '../img/logo/google-black.svg'; // Меняем изображение
+//         Google.style.opacity = 1; // Показываем изображение
+//       }, 300);
+//       telefhoneColor.style.opacity = 0; // Скрываем изображение
+//       setTimeout(() => {
+//         telefhoneColor.src = '../img/scooter/security__telefhone-white.png'; // Меняем изображение
+//         telefhoneColor.style.opacity = 1; // Показываем изображение
+//       }, 300);
+//       littleBikeDown.style.opacity = 0; // Скрываем изображение
+//       setTimeout(() => {
+//         littleBikeDown.src = '../img/scooter/scooter-black.png'; // Меняем изображение
+//         littleBikeDown.style.opacity = 1; // Показываем изображение
+//       }, 300);
+//   } else return
+// })
 
 // ---------------------------------------бургер меню
 
@@ -201,34 +297,6 @@ items2.forEach(item => {
 });
 
 // ---------------------------------слайдер с отзывами
-
-// const slides = document.querySelectorAll('.feedback__slide-content');
-// const nextButton = document.getElementById('next');
-// const prevButton = document.getElementById('prev');
-// let currentSlide = 0;
-
-// function updateSlidePosition() {
-//     const slidesContainer = document.querySelector('.feedback__slide');
-//     slidesContainer.style.transform = `translateX(-${currentSlide * 43}%)`;
-// }
-
-// nextButton.addEventListener('click', () => {
-//     if (currentSlide < slides.length - 1) {
-//         currentSlide++;
-//     } else {
-//         currentSlide = 0; // Циклический переход
-//     }
-//     updateSlidePosition();
-// });
-
-// prevButton.addEventListener('click', () => {
-//     if (currentSlide > 0) {
-//         currentSlide--;
-//     } else {
-//         currentSlide = slides.length - 1; // Циклический переход
-//     }
-//     updateSlidePosition();
-// });
 
 const slides = document.querySelectorAll('.feedback__slide-content');
 const nextButton = document.getElementById('next');
